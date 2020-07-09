dd33e52ce6ecf3cfaff9bc38279813e74eb28b53
Chris Chatzi <[Your>
2020-07-10 00:47:27 +0300

    Personal Website Nr.3

---
+++ script.js
@@ -0,0 +1,37 @@
+const about = document.querySelector(".about");
+const exp_edu = document.querySelector(".exp_edu");
+const contact = document.querySelector(".contact");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
+
+menuNav.addEventListener("click", (e) => {
+  navItems.forEach((item) => {
+    item.classList.remove("current");
+  });
+  e.target.classList.add("current");
+  if (e.target.classList.contains("about")) {
+    first.classList.add("show");
+    first.classList.remove("hide");
+    second.classList.add("hide");
+    second.classList.remove("show");
+    third.classList.add("hide");
+    third.classList.remove("show");
+  } else if (e.target.classList.contains("exp_edu")) {
+    first.classList.add("hide");
+    first.classList.remove("show");
+    second.classList.add("show");
+    second.classList.remove("hide");
+    third.classList.add("hide");
+    third.classList.remove("show");
+  } else {
+    first.classList.add("hide");
+    first.classList.remove("show");
+    second.classList.add("hide");
+    second.classList.remove("show");
+    third.classList.add("show");
+    third.classList.remove("hide");
+  }
+});
